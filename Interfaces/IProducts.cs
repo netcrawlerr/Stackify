4da990c16de3100fe981805f332088254ccb9f1a
netcrawlerr <abdulmejidamk@gmail.com>
2024-10-21 14:20:36 +0300

    interfaces

---
+++ Interfaces/IProducts.cs
@@ -0,0 +1,9 @@
+using backend.Models;
+
+namespace backend.Interfaces;
+
+public interface IProducts
+{
+    public Task<List<Products>> GetProductsAsync();
+    public Task<Products> CreateProductsAsync(Products products);
+}
