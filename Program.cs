76930a504d2f9c5277a3079dc78ed9ed76acc017
netcrawlerr <abdulmejidamk@gmail.com>
2024-10-21 13:39:57 +0300

    "Swagger"

--- Program.cs
+++ Program.cs
@@ -2,18 +2,52 @@ using backend.Data;
 using backend.Interfaces;
 using backend.Repository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddControllers();
 
-builder.Services.AddSwaggerGen();
+// builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+    options.AddSecurityDefinition(
+        "Bearer",
+        new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Description = "Please Enter A Valid Token",
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            BearerFormat = "JWT",
+            Scheme = "Bearer",
+        }
+    );
+    options.AddSecurityRequirement(
+        new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer",
+                    },
+                },
+                new string[] { }
+            },
+        }
+    );
+});
 
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
 {
     options.UseMySql(
-        "Server=127.0.0.1;Database=DOTNETCRUD;User=root;Password=netcrawler;",
+        "Server=127.0.0.1;Database=DOTNETCRUD;User=netcrawler;Password=netcrawler;",
         new MySqlServerVersion(new Version(8, 0, 21))
     );
 });
