455c7ac829dfad53e1d42a3948b4479a26f5cedd
netcrawlerr <abdulmejidamk@gmail.com>
2024-10-21 13:40:21 +0300

    "product:actions"

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -2,6 +2,7 @@ using backend.DTO;
 using backend.Interfaces;
 using backend.Mappers;
 using Microsoft.AspNetCore.Mvc;
+using Stackify.DTO;
 
 namespace backend.Controllers
 {
@@ -28,6 +29,18 @@ namespace backend.Controllers
             return Ok(products);
         }
 
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetSingleProduct([FromRoute] int id)
+        {
+            var products = await _products.GetSingleProductsAsync(id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+            return Ok(products.ToProductsDto());
+        }
+
         [HttpPost]
         public async Task<IActionResult?> CreateProducts(
             [FromBody] CreateProductsRequestDto createProductsRequestDto
@@ -42,6 +55,37 @@ namespace backend.Controllers
             await _products.CreateProductsAsync(productModel);
 
             return Ok(productModel);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> UpdateProducts(
+            [FromBody] UpdateProductsRequestDto updateProductsRequestDto,
+            [FromRoute] int id
+        )
+        {
+            var productModel = await _products.UpdateProductsAsync(updateProductsRequestDto, id);
+
+            if (productModel == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(productModel.ToProductsDto());
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> DeleteProducts([FromRoute] int id)
+        {
+            var productModel = await _products.DeleteProductsAsync(id);
+
+            if (productModel == null)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
         }
     }
 }
