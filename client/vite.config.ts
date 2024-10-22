26861242f911a54d2b7fcbfa52d425cc87ccc5ef
netcrawlerr <abdulmejidamk@gmail.com>
2024-10-22 19:52:33 +0300

    "client:init"

---
+++ client/vite.config.ts
@@ -0,0 +1,17 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [react()],
+
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5217/api",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        secure: false,
+      },
+    },
+  },
+});
