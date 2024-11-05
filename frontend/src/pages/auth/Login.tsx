e8117231b9e72e5391c962441214f8eb7c12deb9
netcrawlerr <abdulmejidamk@gmail.com>
2024-11-05 10:09:24 +0300

    client:pages-login-register

---
+++ frontend/src/pages/auth/Login.tsx
@@ -0,0 +1,126 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "../../components/ui/button";
+import { Input } from "../../components/ui/input";
+import { useAuthStore } from "../..//store/auth";
+
+export default function Login() {
+  const navigate = useNavigate();
+  const login = useAuthStore((state) => state.login);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError("");
+    setIsLoading(true);
+
+    try {
+      await login(formData.username, formData.password);
+      navigate("/dashboard");
+    } catch (err) {
+      setError("Invalid username or password");
+      console.log(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-900 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div className="text-center">
+          {/* <BoxesIcon className="mx-auto h-12 w-12 text-blue-600" /> */}
+          <label htmlFor="" className="text-4xl">
+            💼
+          </label>
+          <h2 className="mt-6 text-3xl font-extrabold text-gray-900 dark:text-white">
+            Welcome to Stackify
+          </h2>
+          <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
+            Sign in to your account
+          </p>
+        </div>
+
+        <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+          {error && (
+            <div className="bg-red-50 dark:bg-red-900/50 text-red-500 p-3 rounded-md text-sm">
+              {error}
+            </div>
+          )}
+
+          <div className="space-y-4">
+            <div>
+              <label
+                htmlFor="username"
+                className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+              >
+                Username
+              </label>
+              <Input
+                id="username"
+                name="username"
+                type="text"
+                required
+                value={formData.username}
+                onChange={(e) =>
+                  setFormData({ ...formData, username: e.target.value })
+                }
+                className="mt-1"
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+              >
+                Password
+              </label>
+              <Input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                value={formData.password}
+                onChange={(e) =>
+                  setFormData({ ...formData, password: e.target.value })
+                }
+                className="mt-1"
+              />
+            </div>
+
+            <div className="flex items-center justify-between">
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-medium text-blue-600 hover:text-blue-500"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <Button type="submit" className="w-full" isLoading={isLoading}>
+            Sign in
+          </Button>
+          <div className="text-sm ">
+            <span className="font-medium mr-2 text-stone-600 hover:text-stone-500">
+              Don't have account ?
+            </span>
+            <Link to="/register" className="text-blue-600">
+              Register
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/pages/auth/Register.tsx
@@ -0,0 +1,224 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "../../components/ui/button";
+import { Input } from "../../components/ui/input";
+import { useAuthStore } from "../..//store/auth";
+import { Phone } from "lucide-react";
+
+export default function Register() {
+  const navigate = useNavigate();
+  const register = useAuthStore((state) => state.register);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    username: "",
+    password: "",
+    phoneNumber: "",
+  });
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError("");
+    setIsLoading(true);
+
+    try {
+    //   console.log(formData);
+      await register(
+        formData.firstName,
+        formData.lastName,
+        formData.email,
+        formData.phoneNumber,
+        formData.username,
+        formData.password
+      );
+
+      navigate("/dashboard");
+    } catch (err) {
+      console.log("", err.message);
+
+      setError(err.message);
+      console.log(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-900 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div className="text-center">
+          {/* <BoxesIcon className="mx-auto h-12 w-12 text-blue-600" /> */}
+          <label htmlFor="" className="text-4xl">
+            💼
+          </label>
+          <h2 className="mt-6 text-3xl font-extrabold text-gray-900 dark:text-white">
+            Welcome to Stackify
+          </h2>
+          <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
+            Let's get you Sign Up
+          </p>
+        </div>
+
+        <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+          {error && (
+            <div className="bg-red-50 dark:bg-red-900/50 text-red-500 p-3 rounded-md text-sm">
+              {error}
+            </div>
+          )}
+
+          <div className="space-y-4">
+            <div className="flex space-x-3">
+              <div>
+                <label
+                  htmlFor="firstName"
+                  className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+                >
+                  First Name
+                </label>
+                <Input
+                  id="firstName"
+                  name="firstName"
+                  type="text"
+                  required
+                  value={formData.firstName}
+                  onChange={(e) =>
+                    setFormData({ ...formData, firstName: e.target.value })
+                  }
+                  className="mt-1"
+                />
+              </div>
+
+              <div>
+                <label
+                  htmlFor="lastName"
+                  className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+                >
+                  Last Name
+                </label>
+                <Input
+                  id="lastName"
+                  name="lastName"
+                  type="text"
+                  required
+                  value={formData.lastName}
+                  onChange={(e) =>
+                    setFormData({ ...formData, lastName: e.target.value })
+                  }
+                  className="mt-1"
+                />
+              </div>
+            </div>
+
+            <div className="flex space-x-3">
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+                >
+                  Email
+                </label>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  value={formData.email}
+                  onChange={(e) =>
+                    setFormData({ ...formData, email: e.target.value })
+                  }
+                  className="mt-1"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="phoneNumber"
+                  className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+                >
+                  Phone
+                </label>
+                <Input
+                  id="phoneNumber"
+                  name="phoneNumber"
+                  type="text"
+                  required
+                  value={formData.phoneNumber}
+                  onChange={(e) =>
+                    setFormData({ ...formData, phoneNumber: e.target.value })
+                  }
+                  className="mt-1"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="username"
+                className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+              >
+                Username *
+              </label>
+              <Input
+                id="username"
+                name="username"
+                type="text"
+                required
+                value={formData.username}
+                onChange={(e) =>
+                  setFormData({ ...formData, username: e.target.value })
+                }
+                className="mt-1"
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+              >
+                Password *
+              </label>
+              <Input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                value={formData.password}
+                onChange={(e) =>
+                  setFormData({ ...formData, password: e.target.value })
+                }
+                className="mt-1"
+              />
+            </div>
+
+            <div className="flex items-center justify-between">
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-medium text-blue-600 hover:text-blue-500"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <Button type="submit" className="w-full" isLoading={isLoading}>
+            Sign in
+          </Button>
+          <div className="text-sm ">
+            <span className="font-medium mr-2 text-stone-600 hover:text-stone-500">
+              Don't have account ?
+            </span>
+            <Link to="/register" className="text-blue-600">
+              Register
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
