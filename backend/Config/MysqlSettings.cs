823ef3bb75156cd62c7a065612d45d569cf0da80
netcrawlerr <abdulmejidamk@gmail.com>
2024-11-15 06:29:58 +0300

    "server:removed nullability"

--- backend/Config/MysqlSettings.cs
+++ backend/Config/MysqlSettings.cs
@@ -2,9 +2,9 @@ namespace backend.Config
 {
     public class MysqlSettings
     {
-        public string Server { get; set; }
-        public string Database { get; set; }
-        public string User { get; set; }
-        public string Password { get; set; }
+        public required string Server { get; set; }
+        public required string Database { get; set; }
+        public required string User { get; set; }
+        public required string Password { get; set; }
     }
 }
