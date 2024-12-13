0bb3906f0a3391c75196fff1856983de1316be71
netcrawlerr <abdulmejidamk@gmail.com>
2024-12-13 17:37:07 +0300

    "Dto: create supplier"

---
+++ Stackify.Application/DTO/Supplier/CreateSupplierRequestDto.cs
@@ -0,0 +1,13 @@
+namespace backend.DTO.Supplier;
+
+public class CreateSupplierRequestDto
+{
+    public string SupplierName { get; set; } = string.Empty;
+    public string Category { get; set; } = string.Empty;
+    public decimal Rating { get; set; } = 0;
+    public string Email { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+    public string DateJoined { get; set; } = string.Empty;
+    public string MostSoldItem { get; set; } = string.Empty;
+    public string Status { get; set; } = string.Empty;
+}
