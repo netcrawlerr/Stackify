033e06f00fe0d31157a6a09f47cca7c6b20f94d5
netcrawlerr <abdulmejidamk@gmail.com>
2024-12-13 16:43:36 +0300

    "categories Model"

---
+++ Stackify.Core/Models/Categories.cs
@@ -0,0 +1,8 @@
+namespace backend.Models;
+
+public class Categories
+{
+    public int Id { get; set; }
+    public string CategoryName { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
