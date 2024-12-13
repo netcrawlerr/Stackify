c4e0619b22a35819951b20d164be451c5ed4bdb2
netcrawlerr <abdulmejidamk@gmail.com>
2024-12-13 17:07:29 +0300

    "core: Iproduct"

---
+++ Stackify.Core/Interfaces/IProducts.cs
@@ -0,0 +1,18 @@
+using backend.Models;
+using backend.DTO;
+
+namespace backend.Interfaces;
+
+public interface IProducts
+{
+    public Task<List<Products>> GetProductsAsync();
+    public Task<Products?> GetSingleProductsAsync(int id);
+    public Task<Products?> GetProductByNameAsync(string name);
+    public Task<Products> CreateProductsAsync(Products productsModel);
+    public Task<Products?> UpdateProductsAsync(
+        UpdateProductsRequestDto updateProductsRequestDto,
+        int id
+    );
+
+    public Task<Products?> DeleteProductsAsync(int id);
+}
