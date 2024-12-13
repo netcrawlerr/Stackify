9d3bee30caff46519cb4d3c5b47ece4382330166
netcrawlerr <abdulmejidamk@gmail.com>
2024-12-13 17:36:02 +0300

    "Dto: create category"

---
+++ Stackify.Application/DTO/Catergories/CreateCategoryRequestDto.cs
@@ -0,0 +1,7 @@
+namespace backend.DTO.Catergories;
+
+public class CreateCategoryRequestDto
+{
+    public string CategoryName { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
