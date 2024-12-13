f3d5e78fc426d2036569ddaef5bb4887dcb8925a
netcrawlerr <abdulmejidamk@gmail.com>
2024-12-13 17:36:42 +0300

    "Dto: order & order item"

---
+++ Stackify.Application/DTO/Order/OrderItemDto.cs
@@ -0,0 +1,12 @@
+namespace backend.DTO
+{
+    public class OrderItemDto
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal Total { get; set; }
+    }
+}
