720911681cc4c7c3d93ef33e7d11dc9b273227c1
netcrawlerr <abdulmejidamk@gmail.com>
2024-12-13 17:36:13 +0300

    "Dto: category"

---
+++ Stackify.Application/DTO/Catergories/CategoriesDto.cs
@@ -0,0 +1,9 @@
+namespace backend.DTO
+{
+    public class CategoriesDto
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
