c1673d85dfb3debac6715ac01545abc2370a2c3d
netcrawlerr <abdulmejidamk@gmail.com>
2024-12-15 21:25:23 +0300

    "order mapper"

---
+++ Stackify.Application/Mappers/OrderMappers.cs
@@ -0,0 +1,17 @@
+namespace Stackify.Application.Mappers;
+
+public static class OrderMappers
+{
+    public static Order ToOrder(this OrderDto orderDto)
+    {
+        return new Order
+        {
+            Id = orderDto.Id,
+            OrderNumber = orderDto.OrderNumber,
+            CustomerId = orderDto.CustomerId,
+            OrderDate = orderDto.OrderDate,
+            TotalAmount = orderDto.TotalAmount,
+            Status = orderDto.Status,
+        };
+    }
+}
