08b4f54b4ab124e56a9ec1d5d162a33216e1837a
netcrawlerr <abdulmejidamk@gmail.com>
2024-12-15 21:25:48 +0300

    "products mapper"

---
+++ Stackify.Application/Mappers/ProductsMappers.cs
@@ -0,0 +1,28 @@
+namespace Stackify.Application.Mappers;
+
+public static class ProductsMappers
+{
+    public static ProductsDto ToProductsDto(this Products productsModel)
+    {
+        return new ProductsDto
+        {
+            Id = productsModel.Id,
+            Name = productsModel.Name,
+           
+        };
+    }
+
+    public static Products ToProductsFromCreateDto(
+        this CreateProductsRequestDto createProductsRequestDto
+    )
+    {
+        return new Products
+        {
+            Name = createProductsRequestDto.Name,
+            Description = createProductsRequestDto.Description,
+            Price = createProductsRequestDto.Price,
+            StockLevel = createProductsRequestDto.StockLevel,
+            CategoryId = createProductsRequestDto.CategoryId,
+        };
+    }
+}
