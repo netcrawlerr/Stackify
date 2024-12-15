cbdab038195f5f579aec3b7e44d3ec58bddaca7c
netcrawlerr <abdulmejidamk@gmail.com>
2024-12-15 21:26:38 +0300

    "order order item products repo"

---
+++ Stackify.Application/Repository/OrderItemRepository.cs
@@ -0,0 +1,23 @@
+namespace Stackify.Application.Repository;
+
+public class OrderItemRepository : IOrderItem
+{
+    private readonly ApplicationDBContext _context;
+
+    public OrderItemRepository(ApplicationDBContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<OrderItem> CreateOrderItemAsync(OrderItem orderItemModel)
+    {
+        _context.OrderItem.Add(orderItemModel);
+        await _context.SaveChangesAsync();
+        return orderItemModel;
+    }
+
+    public async Task<List<OrderItem>> GetAllOrderItemsByOrderIdAsync(int id)
+    {
+        return await _context.OrderItem.ToListAsync();
+    }
+}
